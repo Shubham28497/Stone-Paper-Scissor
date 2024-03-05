a934d70abdba8facc67b637e6ab923a5eea085bc
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-05 11:33:13 +0530

    add func to gen comp choice

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ let userScore = 0;
 let compScore = 0;
 const choices = document.querySelectorAll(".choice");
 
+const genCompChoice = () => {
+  const option = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * 3);
+  return option[randomIndex];
+};
+
+const playGame = (userChoice) => {
+  console.log("user choice", userChoice);
+  //* generate comp choice
+  const compChoice=genCompChoice()
+  console.log("comp choice",compChoice)
+};
+
 choices.forEach((choice) => {
   choice.addEventListener("click", () => {
-    const choiceId= choice.getAttribute("id")
-    console.log("choice was clicked",choiceId);
+    const userChoice = choice.getAttribute("id");
+    // console.log("choice was clicked", userChoice);
+    playGame(userChoice);
   });
 });
